154c36f01dca300d318c84c231032d19db19fe1c
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-08 16:58:26 -0400

    added Problem3 unfinished

---
+++ Runner/Problem3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem3
+    {
+        long number = 600851475143;
+        public string Run()
+        {
+            int max = 0;
+            for(int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)// && IsPrime(i))
+                {
+                    max = i;
+                }
+            }
+            return max.ToString();
+        }
+    }
+}

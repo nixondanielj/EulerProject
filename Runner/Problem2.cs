161aeab677666af9f3e8dad2e65b54da37fb5672
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-08 16:33:01 -0400

    added problem2

---
+++ Runner/Problem2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem2
+    {
+        public string Run()
+        {
+            return (from i in GenerateFibonacci()
+                    where i % 2 == 0
+                    select i).TakeWhile(i => i < 4000000)
+                    .Sum().ToString();
+        }
+
+        IEnumerable<int> GenerateFibonacci()
+        {
+            int a = 1, b = 2;
+            yield return a;
+            yield return b;
+            while (true)
+            {
+                int swap = b;
+                b += a;
+                a = swap;
+                yield return b;
+            }
+        }
+    }
+}

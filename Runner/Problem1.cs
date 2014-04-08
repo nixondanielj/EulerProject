13feb984ca6508d4c5aab72bbb8805f42e54356e
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-08 16:22:35 -0400

    problem 1 finished

---
+++ Runner/Problem1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem1
+    {
+        public string Run()
+        {
+            return (from i in Enumerable.Range(0, 1000)
+                    where i % 3 == 0 || i % 5 == 0
+                    select i).Sum().ToString();
+        }
+    }
+}

22979b3bd2528259906e7df447e35469a99d1ee6
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-09 14:46:21 -0400

    simplified Problem7

--- ProjectEuler/Problem7.cs
+++ ProjectEuler/Problem7.cs
@@ -13,13 +13,5 @@ namespace ProjectEuler
             return PrimeHelper.GeneratePrimes()
                 .Take(10001).Last().ToString();
         }
-
-        public IEnumerable<int> InfiniteRange(int start = 0)
-        {
-            for (; true; start++)
-            {
-                yield return start;
-            }
-        }
     }
 }

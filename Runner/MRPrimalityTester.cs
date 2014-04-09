26b3532bc29bd2765f6db9fdfb10412c0bcf86b1
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-09 08:31:32 -0400

    added primality tester

---
+++ Runner/MRPrimalityTester.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class MRPrimalityTester
+    {
+        public static bool IsPrime(long n)
+        {
+            return IsPrime(n, 10);
+        }
+        public static bool IsPrime(long n, int k)
+        {
+            bool isPrime = true;
+            if ((isPrime = n >= 2) && n != 2 && n != 3 && (isPrime = n % 2 != 0))
+            {
+                long s = n - 1;
+                while (s % 2 == 0)
+                {
+                    s >>= 1;
+                }
+                Random rng = new Random();
+                for (int i = 0; i < k; i++)
+                {
+                    BigInteger temp = s,
+                        a = rng.Next(3, n > int.MaxValue ? int.MaxValue : (int)n - 1),
+                        mod = BigInteger.ModPow(a, temp, n);
+                    while (temp != n - 1 && mod != 1 && mod != n - 1)
+                    {
+                        mod = (mod * mod) % n;
+                        temp = temp * 2;
+                    }
+                    if (mod != n - 1 && temp % 2 == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+            }
+            return isPrime;
+        }
+    }
+}

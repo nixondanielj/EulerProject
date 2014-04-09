7a06e857df7b7cc228b5291b3c81502bbde4e331
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-09 08:38:51 -0400

    added Problem 4

---
+++ Runner/Problem4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem4
+    {
+        public string Run()
+        {
+            int max = 0;
+            for (int i = 999; i > 0; i--)
+            {
+                for (int j = 999; j > 0; j--)
+                {
+                    int product = i * j;
+                    if (product > max && IsPalindrome(product))
+                    {
+                        max = product;
+                    }
+                }
+            }
+            return max.ToString();
+        }
+
+        public bool IsPalindrome(int n)
+        {
+            return n.ToString() == new string(n.ToString().Reverse().ToArray());
+        }
+    }
+}

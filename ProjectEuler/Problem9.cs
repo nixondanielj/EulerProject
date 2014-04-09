c6928e9cea6b4af81fb281108922d51ed6f1a5ea
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-09 14:40:17 -0400

    added problem 9

---
+++ ProjectEuler/Problem9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem9
+    {
+        public string Run()
+        {
+            for (int b = 1; true; b++)
+            {
+                for (int a = 1; a < b; a++)
+                {
+                    double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+                    if (a + b + c == 1000)
+                    {
+                        return (a * b * c).ToString();
+                    }
+                }
+            }
+        }
+    }
+}

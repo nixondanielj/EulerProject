9c93e1c35cd55e0c4d23d51664050ce9d30003e6
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-09 08:52:41 -0400

    added Problem5

---
+++ ProjectEuler/Problem5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem5
+    {
+        public string Run()
+        {
+            for (int product = 21; true; product++)
+            {
+                if (Enumerable.Range(2, 20).All(d => product % d == 0))
+                {
+                    return product.ToString();
+                }
+            }
+        }
+    }
+}

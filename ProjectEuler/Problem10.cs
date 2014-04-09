67e9719b01cca8c34471ec13637109f1d72fbf7f
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-09 14:51:17 -0400

    added problem 10

---
+++ ProjectEuler/Problem10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem10
+    {
+        public string Run()
+        {
+            long l = 0;
+            return PrimeHelper.GeneratePrimes()
+                .TakeWhile(p => p < 2000000)
+                .Aggregate(l, (a,b)=> a+b)
+                .ToString();
+        }
+    }
+}

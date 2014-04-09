b1a986beaab835ccef1d0082094253fad93aaddf
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-09 09:21:43 -0400

    added problem 6

---
+++ ProjectEuler/Problem6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem6
+    {
+        public string Run()
+        {
+            return (Math.Pow(Enumerable.Range(1, 100).Sum(), 2) - Enumerable.Range(1, 100).Select(x => Math.Pow(x, 2)).Sum()).ToString();
+        }
+    }
+}

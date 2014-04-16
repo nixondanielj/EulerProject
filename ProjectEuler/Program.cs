ca8cbe0af8896c6ba85fada6da9ec7dbb89980a9
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-16 16:27:29 -0400

    added tests option 0

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -9,29 +9,74 @@ namespace ProjectEuler
 {
     public class Program
     {
+        static Dictionary<string, string> tests = new Dictionary<string, string>()
+        {
+            {"1", "233168"},
+            {"2","4613732"},
+            {"3", "6857"},
+            {"4", "906609"},
+            {"5", "232792560"},
+            {"6", "25164150"},
+            {"7", "104743"},
+            {"8", "40824"},
+            {"9", "31875000"},
+            {"10", "142913828922"},
+            {"11", "70600674"},
+            {"12", "76576500"}
+        };
         public static void Main(string[] args)
         {
-            var stopwatch = new Stopwatch();
             while (true)
             {
                 string puzzleName = string.Format("Problem{0}",
                     PromptForInt("Enter the problem number:"));
-                var objHandle = GetObjectSafely(puzzleName);
-                if (objHandle != null)
+                if (puzzleName == "Problem0")
                 {
-                    stopwatch.Start();
-                    Console.WriteLine("Result: {0}",
-                        objHandle.GetType().GetMethod("Run").Invoke(objHandle, null));
-                    stopwatch.Stop();
-                    Console.WriteLine("Took {0} seconds", stopwatch.Elapsed.TotalSeconds);
-                    stopwatch.Reset();
+                    RunTests();
                 }
                 else
                 {
-                    Console.WriteLine("{0} was not a valid puzzle", puzzleName);
+                    ExecuteProblem(puzzleName);
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
                 }
-                Console.WriteLine("Press any key to continue");
-                Console.ReadKey();
+            }
+        }
+
+        private static void ExecuteProblem(string puzzleName)
+        {
+            var stopwatch = new Stopwatch();
+            var obj = GetObjectSafely(puzzleName);
+            if (obj != null)
+            {
+                stopwatch.Start();
+                Console.WriteLine("Result: {0}",
+                    obj.GetType().GetMethod("Run").Invoke(obj, null));
+                stopwatch.Stop();
+                Console.WriteLine("Took {0} seconds", stopwatch.Elapsed.TotalSeconds);
+                stopwatch.Reset();
+            }
+            else
+            {
+                Console.WriteLine("{0} was not a valid puzzle", puzzleName);
+            }
+        }
+
+        private static void RunTests()
+        {
+            foreach (var pair in tests)
+            {
+                var obj = GetObjectSafely(
+                    string.Format("Problem{0}", pair.Key)
+                    );
+                if (obj.GetType().GetMethod("Run").Invoke(obj, null).ToString() == pair.Value)
+                {
+                    Console.WriteLine("Problem {0} passed", pair.Key);
+                }
+                else
+                {
+                    Console.WriteLine("Problem {0} failed", pair.Key);
+                }
             }
         }
 

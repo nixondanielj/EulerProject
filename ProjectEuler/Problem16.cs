88c2e93257fc0cfbd4e43c43aabea6091d0e85c0
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-04-21 16:29:40 -0400

    added problem 16

---
+++ ProjectEuler/Problem16.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    class Problem16
+    {
+        public string Run()
+        {
+            // what would we do without BigInteger?
+            // probably write our own implementation...
+            return BigInteger.Pow(2, 1000).ToString()
+                .Select(c => char.GetNumericValue(c)).Sum().ToString();
+        }
+    }
+}

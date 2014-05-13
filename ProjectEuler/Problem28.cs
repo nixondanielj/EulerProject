f46519c0105c5c06770aa1f879eee2be57e80d76
Dan <daniel.nixon@onshoreoutsourcing.com>
2014-05-13 11:21:37 -0400

    added problem 28

---
+++ ProjectEuler/Problem28.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem28
+    {
+        public string Run()
+        {
+            List<int> diagonals = new List<int>(){ 1 };
+            int skipped = 0;
+            for (int i = 3; diagonals.Count < 2001; i+=2)
+            {
+                int toSkip = (diagonals.Count - 1) / 4;
+                if (skipped == toSkip)
+                {
+                    skipped = 0;
+                    diagonals.Add(i);
+                }
+                else
+                {
+                    skipped++;
+                }
+            }
+            return diagonals.Sum().ToString();
+        }
+    }
+}
